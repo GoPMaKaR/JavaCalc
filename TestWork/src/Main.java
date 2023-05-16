cdd72c83aa8f67b12bc182a0953b1676badbd5b5
GoPMaKaR <32206905+GoPMaKaR@users.noreply.github.com>
2023-05-16 14:10:34 +0300

    Update Main.java

--- TestWork/src/Main.java
+++ TestWork/src/Main.java
@@ -1,5 +1,3 @@
-import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -13,49 +11,50 @@ public class Main {
         try {
             Scanner sc = new Scanner(System.in);
             String expression = sc.nextLine();
+            int length = expression.length();
 
-            int firstArg = Integer.parseInt(expression.split(" ")[0]);
-            if (firstArg == 10) {
-                System.out.println("Пожалуйста, укажите цифру 10 вторым числом. Введите число от 1 до 10");
-                return;
-            }
-            if (firstArg < 0 || firstArg > 10) {
-                System.out.println("Вы ввели число меньше или больше указаного диапазона чисел.");
-                return;
-            }
-            int secondArg = Integer.parseInt(expression.split(" ")[2]);
+            if (length <= 6) {
 
-            if (secondArg < 0 || secondArg > 10) {
-                System.out.println("Вы ввели число меньше или больше указаного диапазона чисел.");
-                return;
-            }
+                int firstArg = Integer.parseInt(expression.split(" ")[0]);
+                if (firstArg == 10) {
+                    System.out.println("Пожалуйста, укажите цифру 10 вторым числом. Введите число от 1 до 10");
+                    return;
+                }
+                if (firstArg < 0 || firstArg > 10) {
+                    System.out.println("Вы ввели число меньше или больше указаного диапазона чисел. Введите числа от 1 до 10");
+                    return;
+                }
+                int secondArg = Integer.parseInt(expression.split(" ")[2]);
 
-            String[] arab = {"10", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-            String[] rome = {"X", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+                if (secondArg < 0 || secondArg > 10) {
+                    System.out.println("Вы ввели число меньше или больше указаного диапазона чисел.");
+                    return;
+                }
 
-            String firstString = expression.split(" ")[0];
-            String secondString = expression.split(" ")[2];
-
-
-            char symbol = expression.charAt(2);
-            if (symbol == '+') {
-                int sumResult = firstArg + secondArg;
-                System.out.println("Ответ: " + sumResult);
+                char symbol = expression.charAt(2);
+                if (symbol == '+') {
+                    int sumResult = firstArg + secondArg;
+                    System.out.println("Ответ: " + sumResult);
+                }
+                if (symbol == '-') {
+                    int subsResult = firstArg - secondArg;
+                    System.out.println("Ответ: " + subsResult);
+                }
+                if (symbol == '/') {
+                    int divResult = firstArg / secondArg;
+                    System.out.println("Ответ: " + divResult);
+                }
+                if (symbol == '*') {
+                    int multiResult = firstArg * secondArg;
+                    System.out.println("Ответ: " + multiResult);
+                }
+            } else {
+                System.out.println("Вы ввели не правильный формат. Правильный формат: a + b, a * b.");
             }
-            if (symbol == '-') {
-                int subsResult = firstArg - secondArg;
-                System.out.println("Ответ: " + subsResult);
-            }
-            if (symbol == '/') {
-                int divResult = firstArg / secondArg;
-                System.out.println("Ответ: " + divResult);
-            }
-            if (symbol == '*') {
-                int multiResult = firstArg * secondArg;
-                System.out.println("Ответ: " + multiResult);
-            }
-        }catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             System.out.println("Вы ввели не правильный формат");
+        } catch (ArrayIndexOutOfBoundsException exe) {
+            System.out.println("Строка не является математической операцией");
         }
     }
-    }
+}
